2030f4884e25b3e0c31f9516f6817fe497c2d6ca
gare15 <72866876+gare15@users.noreply.github.com>
2020-10-21 09:10:56 +0700

    Logika

---
+++ src/pbo/pkg10119911/latihan15/logika/Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10119911.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mochamad Tegar Dwi S
+ * KELAS    : IF10K
+ * NIM      : 10119911 
+ * Deskripsi Program : : Logika
+ */
+public class Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
